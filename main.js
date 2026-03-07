15cb87e166d6fc4eb793ceefe7cdc399914344e6
Tareq <tariqulislamtareq1670@gmail.com>
2026-03-07 12:50:44 +0600

    This Time I Create modal Option and Some issue Solve

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const cardContainer = document.getElementById("card_container");
 const countIssues = document.getElementById("issues_count")
 const loadingSection = document.getElementById("loading");
 const issuesContent = document.getElementById("issues_content");
+const popModalBox = document.getElementById("pop_modal_box");
+const popModalContent = document.getElementById("pop_modal_content");
 
 // Submit Button Event
 submitBtn.addEventListener("click", (e) => {
@@ -85,11 +87,12 @@ const displayIssues = (data) => {
     cardContainer.innerHTML = "";
     data.forEach(item => {
         const div = document.createElement("div");
+        div.onclick = () => getModal(item.id)
         div.classList =`${
-            item.status === "open"?`card shadow-sm border border-t-5 border-success`:`card shadow-sm border border-t-5 border-[#a754f5]`
+            item.status === "open"?`card shadow-sm border-t-5 border-success`:`card shadow-sm border-t-5 border-[#a754f5]`
         }`
         div.innerHTML = `
-                                    <div class="card-body">
+                                    <div  class="card-body">
                                 <div class="card_content">
                                     <div
                                         class="flex justify-between items-center mb-3"
@@ -142,7 +145,7 @@ const displayIssues = (data) => {
                                 </div>
                             </div>
                             <div
-                                class="border border-base-300 p-6 text-[12px] text-[#64748B]"
+                                class="border-t border-base-300 p-6 text-[12px] text-[#64748B]"
                             >
                                 <p>#${item.id} by ${item.author}</p>
                                 <p>${item.updatedAt.slice(0,10)}</p>
@@ -221,8 +224,64 @@ const startLoading = () =>{
 }
 
 const stopLoading = () => {
-        loadingSection.classList.remove("flex")
+    loadingSection.classList.remove("flex")
     loadingSection.classList.add("hidden")
     issuesContent.classList.remove("hidden")
-    issuesContent.classList.add("flex");
+}
+
+
+const getModal = async(id) => {
+    popModalBox.showModal()
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+    displayModal(data.data);
+}
+
+const displayModal = (data) =>{
+    popModalContent.innerHTML = `
+                        <h2 class="text-[24px] font-bold mb-2">Fix broken image upload</h2>
+                    <div class=" flex space-x-6 items-center flex-wrap space-y-2">
+                        <div class="badge badge-success rounded-full text-white itemce">Opened</div>
+                        <ul class="flex list-disc gap-6 text-[12px] text-[#64748B] ml-4 md:m-0">
+                            <li>Opened by <span>Fahim Ahmed</span></li>
+                            <li>22/22/2026  </li>
+                        </ul>
+                    </div>
+                    <div class="labels flex flex-wrap gap-2 mt-[26px] mb-6">
+                                    ${data.labels.map(label =>label === "bug"?`<div class="badge badge-soft border-error/40 badge-error rounded-full"><i class="fa-solid fa-bug"></i> BUG</div>`:label === "enhancement"?`<div class="badge badge-soft border-success/40 badge-success rounded-full"><i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT
+                                    </div>`:label === "help wanted"?`<div class="badge badge-soft border-warning/40 badge-warning rounded-full"><i class="fa-regular fa-life-ring"></i>Help Wanted </div>`:label === "good first issue"?`<div
+                                    class="badge badge-soft border-primary/40 badge-primary rounded-full">
+                                   <i class="fa-solid fa-medal"></i> GOOD FIRST ISSUE
+                                   </div>`:label=== "documentation"?`<div
+                                    class="badge badge-soft border-secondary/40 badge-secondary rounded-full">
+                                   <i class="fa-brands fa-readme"></i> DOCUMENTATION
+                                   </div>`:"").join("")}
+                    </div>
+                    <p class="text-[#64748B] mt-6 mb-[27px]">The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.</p>
+
+                    <div class="bg-base-200 flex gap-[30%] p-4 rounded-md">
+                        <div>
+                            <p class="text-[#64748B]">Assignee:</p>
+                            <h2 class="font-semibold">Fahim Ahmed</h2>
+                        </div>
+                        <div>
+                            <p  class="text-[#64748B]">Priority:</p>
+                            ${
+                                            data.priority === "high"?`<div
+                                            class="badge badge-error text-white rounded-full"
+                                        >
+                                            HEIGH
+                                        </div>`:data.priority === "medium"?`<div
+                                            class="badge badge-warning text-white rounded-full"
+                                        >
+                                            MEDIUM
+                                        </div>`:data.priority === "low"?`<div
+                                            class="badge border border-neutral/30 rounded-full"
+                                        >
+                                            LOW
+                                        </div>`:""
+                                        }
+                        </div>
+                    </div>
+    `
 }
